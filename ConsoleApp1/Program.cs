1faf96c2a4080eeeb3eac2ad98cffc442d651a55
RyseQ1u <460255780@qq.com>
2024-05-11 14:14:52 +0800

    首次提交

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleApp1.Methods;
+using System;
+using System.Collections;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var leet = new Leet1146();
+            var result = leet.Action();
+
+            Console.WriteLine(result);
+        }
+    }
+}
