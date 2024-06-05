281f8b4cdef6d653617668c695229e05e799d412
RyseQ1u <460255780@qq.com>
2024-06-05 13:47:14 +0800

    Leet3072 上传题解  未完成 线段树 todo...

--- LeetConsole/Program.cs
+++ LeetConsole/Program.cs
@@ -8,7 +8,7 @@ namespace LeetCode
     {
         private static void Main(string[] args)
         {
-            var leet = new Leet3067();
+            var leet = new Leet3072();
             var result = leet.Action();
 
             Console.WriteLine(result);
