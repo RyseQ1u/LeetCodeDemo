56ec5a5cbd75935e86652936e6a9bb838b16a6f6
RyseQ1u <460255780@qq.com>
2024-07-16 14:06:45 +0800

    2956

---
+++ LeetConsole/Methods/Easy/Leet2956.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace LeetCode.Methods.Easy
+{
+    /// <summary>
+    /// Leet2956
+    /// </summary>
+    public class Leet2956
+    {
+        public int[] Action()
+        {
+            var input1 = new int[] { 4, 3, 2, 3, 1 };
+            var input2 = new int[] { 2, 2, 5, 2, 3, 6 };
+
+            return FindIntersectionValues(input1, input2);
+        }
+
+        public int[] FindIntersectionValues(int[] nums1, int[] nums2)
+        {
+            var result = new int[2];
+            var hs1 = nums1.ToHashSet();
+            var hs2 = nums2.ToHashSet();
+            foreach (var i in nums1)
+            {
+                if (hs2.Contains(i)) { result[0]++; };
+            }
+            foreach (var i in nums2)
+            {
+                if (hs1.Contains(i)) { result[1]++; };
+            }
+            return result;
+        }
+    }
+}
