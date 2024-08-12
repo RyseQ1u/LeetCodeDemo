3a098db49b6a7b26e449e96f65185bc63f7881d9
RyseQ1u <460255780@qq.com>
2024-08-12 10:09:14 +0800

    676

--- LeetConsole/Program.cs
+++ LeetConsole/Program.cs
@@ -11,7 +11,7 @@ namespace LeetCode
     {
         private static void Main(string[] args)
         {
-            var leet = new Leet3132();
+            var leet = new Leet676();
             var result = leet.Action();
 
             Console.WriteLine(result);
