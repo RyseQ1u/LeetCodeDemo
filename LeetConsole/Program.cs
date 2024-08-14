fa3a8bb0e966de97da84d3903983810b82774409
RyseQ1u <460255780@qq.com>
2024-08-14 10:38:22 +0800

    3152

--- LeetConsole/Program.cs
+++ LeetConsole/Program.cs
@@ -1,4 +1,4 @@
-﻿using ConsoleApp3.Methods;
+﻿using LeetCode.Utilities.Extensions;
 using LeetCode.Methods;
 using LeetCode.Methods.Easy;
 using LeetCode.Methods.Hard;
@@ -11,9 +11,10 @@ namespace LeetCode
     {
         private static void Main(string[] args)
         {
-            var leet = new Leet3151();
-            var result = leet.Action();
+            var leet = new Leet3152();
+            var result = leet.Action().CustomToString();
 
+            //Console.WriteLine(result);
             Console.WriteLine(result);
         }
     }
