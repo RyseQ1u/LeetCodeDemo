b75106f3ba77d5c46cef544ec38a494ed5aebf60
RyseQ1u <460255780@qq.com>
2024-09-18 15:34:53 +0800

    2332 2390

---
+++ LeetConsole/Methods/Middle/Leet2390.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Methods
+{
+    public class Leet2390
+    {
+        public string RemoveStars(string s)
+        {
+            var stack = new Stack<char>();
+            foreach (char c in s)
+            {
+                if (c == '*')
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    stack.Push(c);
+                }
+            }
+            var r = stack.Reverse();
+            return string.Join("", r);
+        }
+
+        /// <summary>
+        /// 使用stringbuilder
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public string RemoveStars2(string s)
+        {
+            StringBuilder res = new StringBuilder();
+            foreach (char c in s)
+            {
+                if (c != '*')
+                {
+                    res.Append(c);
+                }
+                else
+                {
+                    res.Length--;
+                }
+            }
+            return res.ToString();
+        }
+    }
+}
---
+++ LeetTest/MethodTests/Middle/Leet2390_Test.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode.Methods;
+using LeetCode.Methods.Easy;
+using LeetCode.Methods.Middle;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetTest.MethodTests.Middle
+{
+    public class Leet2390_Test
+    {
+        public static IEnumerable<object[]> Data => new[]
+{
+            new object[] { "leet**cod*e","lecoe" },
+            new object[] { "erase*****" , string.Empty },
+        };
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void CheckArr_SumDigitDifferences_RetunSum(string s, string check)
+        {
+            // Arrange
+            var leet = new Leet2390();
+
+            // Act
+            var result = leet.RemoveStars2(s);
+
+            // Assert
+            Assert.Equal(check, result);
+        }
+    }
+}
