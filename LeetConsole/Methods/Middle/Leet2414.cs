181a48e9ca54d5cf51ee95d23721732df57f408c
RyseQ1u <460255780@qq.com>
2024-09-19 11:25:52 +0800

    2414

---
+++ LeetConsole/Methods/Middle/Leet2414.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text.RegularExpressions;
+
+namespace LeetCode.Methods.Middle
+{
+    /// <summary>
+    /// 2414
+    /// </summary>
+    public class Leet2414
+    {
+        public int LongestContinuousSubstring(string s)
+        {
+            var r = 1;
+            var n = 1;
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i] - s[i - 1] == 1)
+                {
+                    n++;
+                    //r = Math.Max(n, r);
+                    if (n > r) r = n;
+                }
+                else
+                {
+                    n = 1;
+                }
+            }
+
+            return r;
+        }
+    }
+}
--- LeetTest/MethodTests/Middle/Leet2414_Test.cs
+++ LeetTest/MethodTests/Middle/Leet2414_Test.cs
@@ -8,23 +8,24 @@ using System.Threading.Tasks;
 
 namespace LeetTest.MethodTests.Middle
 {
-    public class Leet2414_Test
+    public class Leet2024_Test
     {
         public static IEnumerable<object[]> Data => new[]
 {
-            new object[] { "abacaba", 2 },
-            new object[] { "abcde", 5},
+            new object[] { "TTFF", 2,4 },
+            new object[] { "TFFT", 1 ,3},
+            new object[] { "TTFTTFTT", 1 ,5},
         };
 
         [Theory]
         [MemberData(nameof(Data))]
-        public void Check_LongestContinuousSubstring_Retun(string answerKey, int check)
+        public void Check_MaxConsecutiveAnswers_Retun(string answerKey, int k, int check)
         {
             // Arrange
-            var leet = new Leet2414();
+            var leet2024 = new Leet2024();
 
             // Act
-            var result = leet.LongestContinuousSubstring(answerKey);
+            var result = leet2024.MaxConsecutiveAnswers(answerKey, k);
 
             // Assert
             Assert.Equal(check, result);
