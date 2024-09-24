66bddf0b070cfcbfdb86f3c5c5654ca9bfdef07f
RyseQ1u <460255780@qq.com>
2024-09-24 12:00:02 +0800

    2207

---
+++ LeetTest/MethodTests/Middle/Leet2207_Test.cs
@@ -0,0 +1,36 @@
+﻿using LeetCode.Methods.Easy;
+using LeetCode.Methods.Hard;
+using LeetCode.Methods.Middle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetTest.MethodTests.Middle
+{
+    public class Leet2207_Test
+    {
+        public static IEnumerable<object[]> Data => new[]
+{
+            new object[] {"abdcdbc","ac" ,4 },
+            new object[] {"aabb","ab" ,6 },
+        };
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void CheckArr_MaximumSubsequenceCount_RetunSum(string text, string pattern, long check)
+        {
+            // Arrange
+            var leet = new Leet2207();
+            //var input1 = new int[][] { new int[] { 3, 5, 3 }, new int[] { 3, 5, 3 }, new int[] { 3, 5, 3 } };
+            //var input2 = new int[][] { new int[] { 1, 0, 2 }, new int[] { 1, 0, 2 } };
+
+            // Act
+            var result = leet.MaximumSubsequenceCount(text, pattern);
+
+            // Assert
+            Assert.Equal(check, result);
+        }
+    }
+}
