4404b2787d15d6a01a9a828149fe2c7d2b5f7474
RyseQ1u <460255780@qq.com>
2024-09-30 14:59:33 +0800

    1845

---
+++ LeetConsole/Class/SeatManager.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Class
+{
+    /// <summary>
+    /// 1845
+    /// </summary>
+    public class SeatManager2
+    {
+        public Stack<int> stack = new Stack<int>();
+        public Stack<int> tmp = new Stack<int>();
+
+        public SeatManager2(int n)
+        {
+            for (int i = n; i >= 1; i--)
+            {
+                stack.Push(i);
+            }
+        }
+
+        public int Reserve()
+        {
+            return stack.Pop();
+        }
+
+        public void Unreserve(int seatNumber)
+        {
+            while (stack.Count > 0 && seatNumber > stack.Peek())
+            {
+                tmp.Push(stack.Pop());
+            }
+            stack.Push(seatNumber);
+            while (tmp.Count > 0)
+            {
+                stack.Push(tmp.Pop());
+            }
+        }
+    }
+
+    /// <summary>
+    /// 使用优先队列
+    /// </summary>
+    /// <param name="n"></param>
+    public class SeatManager
+    {
+        //记录释放的座位
+        private readonly PriorityQueue<int, int> pq = new();
+
+        private int min = 1;
+
+        public SeatManager(int n)
+        {
+            pq = new PriorityQueue<int, int>();
+            min = 1;
+        }
+
+        public int Reserve()
+        {
+            return pq.Count > 0 ? pq.Dequeue() : min++;
+        }
+
+        public void Unreserve(int seatNumber)
+        {
+            pq.Enqueue(seatNumber, seatNumber);
+        }
+    }
+}
---
+++ LeetTest/MethodTests/Middle/Leet1845_Test.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode.Methods.Easy;
+using LeetCode.Methods.Middle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetTest.MethodTests.Middle
+{
+    public class Leet1845_Test
+    {
+        public static IEnumerable<object[]> Data => new[]
+{
+            new object[] {  2 },
+        };
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void Check_Test_Retun(int check)
+        {
+            // Arrange
+            var leet = new Leet1845();
+
+            // Act
+            var result = leet.Test2();
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
