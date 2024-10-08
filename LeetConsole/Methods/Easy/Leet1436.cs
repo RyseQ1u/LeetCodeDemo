d566998b5e631c4a1d9346379ec73f919fc90a1e
RyseQ1u <460255780@qq.com>
2024-10-08 14:57:28 +0800

    1436 983 todo...

---
+++ LeetConsole/Methods/Easy/Leet1436.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace LeetCode.Methods.Easy
+{
+    /// <summary>
+    /// 1436
+    /// </summary>
+    public class Leet1436
+    {
+        public string DestCity(List<List<string>> paths)
+        {
+            var head = new HashSet<string>();
+            var tail = new HashSet<string>();
+            foreach (var path in paths)
+            {
+                head.Add(path.First());
+                tail.Add(path.Last());
+            }
+            return tail.Except(head).First();
+        }
+    }
+}
