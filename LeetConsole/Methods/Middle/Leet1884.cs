71ab04ac1c9db1e9a0413235c3e22bf15a90b4a3
RyseQ1u <460255780@qq.com>
2024-10-14 16:13:12 +0800

    1884

---
+++ LeetConsole/Methods/Middle/Leet1884.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Class;
+using System;
+
+namespace LeetCode.Methods.Middle
+{
+    /// <summary>
+    /// 1884
+    /// </summary>
+    public class Leet1884
+    {
+        /// <summary>
+        /// 数学法 解方程
+        /// 最小的情况
+        /// x+(x-1)+(x-2)...+2+1>=n
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int TwoEggDrop(int n)
+        {
+            return (int)Math.Ceiling((Math.Sqrt(n * 8 + 1) - 1) / 2);
+        }
+    }
+}
