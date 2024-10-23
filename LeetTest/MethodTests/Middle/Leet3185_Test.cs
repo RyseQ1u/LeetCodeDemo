ccbbdfbfc8eba640de0e846fccbc1581a52ae22c
RyseQ1u <460255780@qq.com>
2024-10-23 10:23:50 +0800

    3185

---
+++ LeetTest/MethodTests/Middle/Leet3185_Test.cs
@@ -0,0 +1,33 @@
+﻿using LeetCode.Methods.Easy;
+using LeetCode.Methods.Middle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetTest.MethodTests.Middle
+{
+    public class Leet3185_Test
+    {
+        public static IEnumerable<object[]> Data => new[]
+        {
+            new object[] {new int[] { 12, 12, 30, 24, 24 },2 },
+            new object[] {  new int[] { 72, 48, 24, 3 } ,3 },
+        };
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void CheckArr_CountCompleteDayPairs_RetunSum(int[] input1, long expected)
+        {
+            // Arrange
+            var leet = new Leet3185();
+
+            // Act
+            var result = leet.CountCompleteDayPairs(input1);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
