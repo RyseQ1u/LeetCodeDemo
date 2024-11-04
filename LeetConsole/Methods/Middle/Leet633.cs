e5eac9217b587a34e6e1e56056310ff9c5da74c4
RyseQ1u <460255780@qq.com>
2024-11-04 11:08:18 +0800

    633

---
+++ LeetConsole/Methods/Middle/Leet633.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Methods.Middle
+{
+    /// <summary>
+    /// 633
+    /// </summary>
+    public class Leet633
+    {
+        public bool JudgeSquareSum(int c)
+        {
+            //mod 4 判断c是否有整数解
+            if (c % 4 == 3) return false;
+            //a b 最大值不超过 c的平方根
+            var limit = Math.Floor(Math.Sqrt(c)) + 1;
+            for (int i = 0; i < limit; i++)
+            {
+                var b_squared = c - i * i;
+                var b = Math.Floor(Math.Sqrt(b_squared));
+                if (b * b == b_squared)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// 直接枚举 双指针
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public bool JudgeSquareSum2(int c)
+        {
+            int a = 0;
+            int b = (int)Math.Sqrt(c);
+            while (a <= b)
+            {
+                if (a * a == c - b * b)
+                { // 避免溢出
+                    return true;
+                }
+                if (a * a < c - b * b)
+                {
+                    a++;
+                }
+                else
+                {
+                    b--;
+                }
+            }
+            return false;
+        }
+    }
+}
