899e6d32f7ae52be73716b524bca563571a6e46c
RyseQ1u <460255780@qq.com>
2024-11-06 11:35:43 +0800

    3254

---
+++ LeetConsole/Methods/Middle/Leet3254.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace LeetCode.Methods.Middle
+{
+    /// <summary>
+    /// 3254
+    /// </summary>
+    public class Leet3254
+    {
+        /// <summary>
+        /// 统计个数
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public int[] ResultsArray(int[] nums, int k)
+        {
+            var res = new int[nums.Length - k + 1];
+            Array.Fill(res, -1);
+            //递增次数
+            int cnt = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                //是否递增
+                if (cnt == 0 || nums[i] == nums[i - 1] + 1)
+                {
+                    cnt++;
+                }
+                else
+                {
+                    cnt = 1;
+                }
+                if (cnt >= k)
+                {
+                    res[i - k + 1] = nums[i];
+                }
+            }
+            return res;
+        }
+    }
+}
