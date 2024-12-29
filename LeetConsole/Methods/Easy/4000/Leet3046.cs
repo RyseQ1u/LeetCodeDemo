5b523a423b5105a91da4632f22b6decb6a9ad16f
RyseQ1u <460255780@qq.com>
2024-12-29 23:56:57 +0800

    1366 3046

---
+++ LeetConsole/Methods/Easy/4000/Leet3046.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Methods.Easy
+{
+    /// <summary>
+    /// 3046
+    /// </summary>
+    public class Leet3046
+    {
+        public bool IsPossibleToSplit(int[] nums)
+        {
+            var res = true;
+            //记录数字出现次数
+            var dict = new Dictionary<int, int>();
+            foreach (var i in nums)
+            {
+                if (dict.ContainsKey(i))
+                {
+                    if (dict[i] == 2)
+                    {
+                        res = false;
+                        break;
+                    }
+                    dict[i]++;
+                }
+                else
+                {
+                    dict[i] = 1;
+                }
+            }
+            return res;
+        }
+
+        /// <summary>
+        /// 题目num的范围是0-100
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public bool IsPossibleToSplit_V2(int[] nums)
+        {
+            var res = true;
+            //记录数字出现次数
+            var dict = new int[101];
+            foreach (var i in nums)
+            {   
+                if (++dict[i] == 3)
+                {
+                    res = false;
+                    break;
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ LeetConsole/Methods/Middle/2000/Leet1366.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Methods.Middle
+{
+    /// <summary>
+    /// 1366
+    /// </summary>
+    public class Leet1366
+    {
+        public string RankTeams(string[] votes)
+        {
+            var dict = new Dictionary<char, int[]>();
+            foreach (char vid in votes[0])
+            {
+                dict[vid] = new int[votes[0].Length];
+            }
+
+            //遍历统计
+            foreach (var vot in votes)
+            {
+                for (var i = 0; i < vot.Length; i++)
+                {
+                    dict[vot[i]][i]++;
+                }
+            }
+            //取出全部的keys
+            var keys = dict.Keys.ToArray();
+            Array.Sort(keys);
+            for (int i = 0; i < keys.Length - 1; i++)
+            {
+                for (int j = 0; j < keys.Length - i - 1; j++)
+                {
+                    if (Compare(dict[keys[j + 1]], dict[keys[j]]))
+                    {
+                        (keys[j + 1], keys[j]) = (keys[j], keys[j + 1]);
+                    }
+                }
+            }
+
+            return string.Concat(keys);
+        }
+
+        private bool Compare(int[] arr1, int[] arr2)
+        {
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if (arr1[i] > arr2[i])
+                {
+                    return true;
+                }
+                else if (arr1[i] == arr2[i])
+                {
+                    continue;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+    }
+}
