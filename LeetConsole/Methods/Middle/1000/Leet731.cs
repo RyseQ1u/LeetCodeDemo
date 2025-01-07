e33a98dfdc54400f905bfa663b935a4590025c71
RyseQ1u <460255780@qq.com>
2025-01-07 19:59:52 +0800

    3019

--- LeetConsole/Methods/Middle/1000/Leet731.cs
+++ LeetConsole/Methods/Middle/1000/Leet731.cs
@@ -23,11 +23,12 @@ namespace LeetCode.Methods.Middle
         /// <summary>
         /// 记录起终点 和 预定次数
         /// </summary>
-        private List<(int s, int e, int[] n)> DateList { get; set; } = new List<(int, int, int[])>();
 
         public MyCalendarTwo()
         {
         }
+
+        private List<int[]> DateList { get; set; } = new List<int[]>();
 
         public bool Book(int startTime, int endTime)
         {
@@ -39,9 +40,9 @@ namespace LeetCode.Methods.Middle
                 //判断两个数组是否相交
                 //区间1的起点小于或等于区间2的终点 且 区间2的起点小于或等于区间1的终点
 
-                if (DateList[i].s < endTime && startTime < DateList[i].e)
+                if (DateList[i][0] < endTime && startTime < DateList[i][1])
                 {
-                    if (DateList[i].n[0] == 2)
+                    if (DateList[i][2] == 2)
                     {
                         f = false;
                         break;
@@ -49,13 +50,13 @@ namespace LeetCode.Methods.Middle
                     else
                     {
                         n++;
-                        DateList[i].n[0]++;
+                        DateList[i][2]++;
                     }
                 }
             }
             if (f)
             {
-                DateList.Add((startTime, endTime, new int[1] { n }));
+                DateList.Add(new int[3] { startTime, endTime, 1 });
             }
 
             return f;
