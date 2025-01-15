d508450bf776ceff5b96c6df213829cf2ab88b6a
RyseQ1u <460255780@qq.com>
2025-01-15 10:14:08 +0800

    3066

---
+++ LeetConsole/Methods/Middle/4000/Leet3066.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Methods.Middle
+{
+    /// <summary>
+    /// 3066
+    /// </summary>
+    public class Leet3066
+    {
+        /// <summary>
+        /// 使用优先队列
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public int MinOperations_V2(int[] nums, int k)
+        {
+            int res = 0;
+            PriorityQueue<long, long> pq = new PriorityQueue<long, long>();
+            foreach (int num in nums)
+            {
+                pq.Enqueue(num, num);
+            }
+            while (pq.Peek() < k)
+            {
+                long x = pq.Dequeue(), y = pq.Dequeue();
+                long a = (long)Math.Min(x, y) * 2 + Math.Max(x, y);
+                pq.Enqueue(a, a);
+                res++;
+            }
+            return res;
+        }
+
+        /// <summary>
+        /// 超时
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public int MinOperations(int[] nums, int k)
+        {
+            var res = 0;
+            var l = new List<long>();
+            var r = new List<long>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] < k)
+                {
+                    l.Add(nums[i]);
+                }
+                else
+                {
+                    r.Add(nums[i]);
+                }
+            }
+            l.Sort();
+            r.Sort();
+            while (l.Count > 1)
+            {
+                long t = l[0] * 2 + l[1];
+                l.RemoveAt(0);
+                l.RemoveAt(0);
+                if (t < k)
+                {
+                    l.Add(t);
+                    l.Sort();
+                }
+                else
+                {
+                    r.Add(t);
+                    r.Sort();
+                }
+                res++;
+            }
+            if (l.Count == 1)
+            {
+                res++;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ LeetTest/MethodTests/Middle/Leet3066_Test.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Methods.Middle;
+
+namespace LeetTest.MethodTests.Middle
+{
+    public class Leet3066_Test
+    {
+        public static IEnumerable<object[]> Data => new[]
+{
+            new object[] { new int[]{ 2, 11, 10, 1, 3 }, 10,2 },
+            new object[] { new int[]{ 1, 1, 2, 4, 9 }, 20,4 },
+            new object[] { new int[]{ 999999999, 999999999, 999999999 }, 1000000000, 2 },
+            new object[] { new int[]{ 19, 44, 61, 96, 84, 79, 84, 61, 97, 44 }, 44, 1 },
+        };
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void Check_MinOperations_Retun(int[] nums, int k, int check)
+        {
+            // Arrange
+            var leet = new Leet3066();
+
+            // Act
+            var result = leet.MinOperations(nums, k);
+
+            // Assert
+            Assert.Equal(check, result);
+        }
+    }
+}
