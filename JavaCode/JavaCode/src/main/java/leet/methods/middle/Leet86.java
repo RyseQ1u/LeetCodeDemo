61a9d36b7d7d74f2cba8f86dc18e2f8e2e0da144
Ryse Q1u <460255780@qq.com>
2025-03-05 13:38:18 +0800

    86

---
+++ JavaCode/JavaCode/src/main/java/leet/methods/middle/Leet86.java
@@ -0,0 +1,35 @@
+package leet.methods.easy;
+
+import leet._class.ListNode;
+
+public class Leet86 {
+
+    /// 分隔链表
+    public ListNode partition(ListNode head, int x) {
+        //创建两个链表存储数据
+        // 记录两个链表起节点
+        ListNode smallFirst = new ListNode(0), bigFirst = new ListNode(0);
+        ListNode small = smallFirst,big = bigFirst;
+        while (head != null) {
+            //当前节点比 x 小
+            if (head.val < x) {
+                //进入 small 链表
+                small.next = head;
+                small = small.next;
+            }else {
+                //进入 big 链表
+                big.next = head;
+                big = big.next;
+            }
+            //递归遍历节点
+            head = head.next;
+        }
+        //连接两个链表
+        small.next = bigFirst.next;
+        //big链表收尾
+        big.next = null;
+        //返回结果
+        return small.next;
+    }
+    
+}
---
+++ JavaCode/JavaCode/src/test/java/methods/middle/Leet86Test.java
@@ -0,0 +1,36 @@
+
+import leet._class.ListNode;
+import leet.methods.middle.Leet86;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class Leet86Test {
+
+    // 方式 2：使用 MethodSource 传递复杂参数
+    @ParameterizedTest
+    @MethodSource("provideTestCases")
+    void testLeet206WithMethodSource(ListNode l1, int x, ListNode expected) {
+        ListNode res = new Leet86().partition(l1,x);
+        Assertions.assertEquals (expected, res);
+    }
+
+    // 参数生成方法（必须为 static）
+    private static Stream<Arguments> provideTestCases() {
+        return Stream.of(
+                Arguments.of(
+                        new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(1)))),
+                        3,
+                        new ListNode(1,new ListNode(2,new ListNode(1,new ListNode(3)))
+
+
+
+                )
+
+        );
+    }
+
+}
