12596ce56db484a3d79e8dcf551b0ef7658c8d21
RyseQ1u <460255780@qq.com>
2025-03-10 19:44:31 +0800

    leet115 commit

--- JavaCode/JavaCode/src/main/java/leet/_class/stackAndqueue/ListNode_Leet115.java
+++ JavaCode/JavaCode/src/main/java/leet/_class/stackAndqueue/ListNode_Leet115.java
@@ -1,12 +1,16 @@
-package leet._class.linklist;
+package leet._class.stackAndqueue;
 
- public class ListNode {
+ public class ListNode_Leet115 {
      public int val;
      /// 子节点
-     public ListNode next;
-     public ListNode() {}
-     public ListNode(int val) { this.val = val; }
-     public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     public ListNode_Leet115 next;
+     /// 父节点
+     public ListNode_Leet115 pre;
+     /// 最小节点
+     public ListNode_Leet115 min;
+     public ListNode_Leet115() {}
+     public ListNode_Leet115(int val) { this.val = val; }
+     public ListNode_Leet115(int val, ListNode_Leet115 next) { this.val = val; this.next = next; }
 
      // 重写 equals 方法
      @Override
@@ -15,8 +19,8 @@ package leet._class.linklist;
          if (this == o) return true;
          if (o == null || getClass() != o.getClass()) return false;
 
-         ListNode current = this;
-         ListNode other = (ListNode) o;
+         ListNode_Leet115 current = this;
+         ListNode_Leet115 other = (ListNode_Leet115) o;
 
          // 2. 逐个节点迭代比较
          while (current != null && other != null) {
---
+++ JavaCode/JavaCode/src/test/java/methods/linklist/middle/Leet115Test.java
@@ -0,0 +1,35 @@
+package methods.linklist.middle;
+
+import leet._class.linklist.ListNode;
+import leet.methods.linklist.middle.Leet237;
+import leet.stackAndqueue.easy.Leet115;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class Leet115Test {
+
+    // 方式 2：使用 MethodSource 传递复杂参数
+    @ParameterizedTest
+    @MethodSource("provideTestCases")
+    void testLeet237WithMethodSource( Integer[] expected) {
+        Integer[] arr = new Leet115().Test();
+        Assertions.assertArrayEquals (expected,arr);
+    }
+
+    // 参数生成方法（必须为 static）
+    private static Stream<Arguments> provideTestCases() {
+        Integer[] arr =  {null,null,null,null,-3,null,0,-2};
+        return Stream.of(
+                Arguments.of(
+                        arr
+
+                )
+
+        );
+    }
+
+}
