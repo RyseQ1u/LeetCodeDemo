a757316554b897f48514a6c1e31d183c9513521a
RyseQ1u <460255780@qq.com>
2025-03-25 10:37:41 +0800

    Leet160 commit

--- JavaCode/JavaCode/src/main/java/leet/Daily/easy/Leet2255.java
+++ JavaCode/JavaCode/src/main/java/leet/Daily/easy/Leet2255.java
@@ -1,30 +1,26 @@
 package leet.Daily.easy;
 
-import leet._class.linklist.ListNode;
-
-import java.util.Arrays;
-
 public class Leet2255 {
 
 
     public int countPrefixes(String[] words, String s) {
-        int res =0;
+        int res = 0;
         //遍历数组 比较位上的字符
         boolean[] flag = new boolean[words.length];
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             for (int j = 0; j < words.length; j++) {
-                if(flag[j] ) continue;
-                if(words[j].length()>s.length()) {
-                    flag[j]=true;
+                if (flag[j]) continue;
+                if (words[j].length() > s.length()) {
+                    flag[j] = true;
                     continue;
                 }
-                if(words[j].length()>i){
-                   if(c == words[j].charAt(i)){
-                       flag[j]=false;
-                   }else {
-                       flag[j]=true;
-                   }
+                if (words[j].length() > i) {
+                    if (c == words[j].charAt(i)) {
+                        flag[j] = false;
+                    } else {
+                        flag[j] = true;
+                    }
                 }
             }
         }
@@ -33,7 +29,6 @@ public class Leet2255 {
         }
         return res;
     }
-
 
 
 }
