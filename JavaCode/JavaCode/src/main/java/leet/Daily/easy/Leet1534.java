4d830b1231a9a016f2cc53f03ab165940aa0e594
RyseQ1u <460255780@qq.com>
2025-04-14 09:41:55 +0800

    Leet1534  commit

---
+++ JavaCode/JavaCode/src/main/java/leet/Daily/easy/Leet1534.java
@@ -0,0 +1,26 @@
+package leet.Daily.easy;
+
+public class Leet1534 {
+
+    /**
+     * 暴力破解
+     * @param
+     * @return
+     */
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int n = arr.length;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (Math.abs(arr[i] - arr[j]) <= a && Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[i] - arr[k]) <= c) {
+                        ans++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+
+}
