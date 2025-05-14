74642f40d5c54bb3bd69e3f1e314499ce9ba3ba6
RyseQ1u <460255780@qq.com>
2025-05-14 11:13:00 +0800

    Leet151 method2  commit

--- JavaCode/JavaCode/src/main/java/leet/methods/doublePointer/middle/Leet151.java
+++ JavaCode/JavaCode/src/main/java/leet/methods/doublePointer/middle/Leet151.java
@@ -20,5 +20,27 @@ public class Leet151 {
         return s2.toString().trim();
     }
 
-
+    /**
+     * 双指针切割
+     * @param s
+     * @return
+     */
+    public String reverseWords2(String s) {
+        // 删除首尾空格 从第一个单词开始
+        s = s.trim();
+        int right = s.length() - 1, left = right;
+        StringBuilder res = new StringBuilder();
+        while (left >= 0) {
+            // 搜索首个空格
+            while (left >= 0 && s.charAt(left) != ' ') left--;
+            // 添加单词
+            res.append(s.substring(left + 1, right + 1) + " ");
+            // 跳过单词间空格 找到下一个单词
+            while (left >= 0 && s.charAt(left) == ' ') left--;
+            // right 指向下个单词的尾字符
+            right = left;
+        }
+        // 转化为字符串并返回
+        return res.toString().trim();
+    }
 }
