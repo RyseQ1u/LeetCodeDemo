a133629b5fc903ed944c74f112ff21748e9ff6ea
RyseQ1u <460255780@qq.com>
2025-06-11 10:11:54 +0800

    Leet278  commit

---
+++ JavaCode/JavaCode/src/main/java/leet/methods/binarySearch/easy/Leet278.java
@@ -0,0 +1,25 @@
+package leet.methods.binarySearch.easy;
+
+public class Leet278 {
+
+    public int firstBadVersion(int n) {
+        int l=1,r=n,i=0;
+        while (l<=r){
+            //防止整型数据溢出
+            i = (r+l)/2;
+            if(isBadVersion(i)){
+                r = i-1;
+            }else {
+                l = i+1;
+            }
+        }
+        return i;
+    }
+
+    private boolean isBadVersion(int n) {
+        if(n>=1) return true;
+        return false;
+    }
+
+
+}
---
+++ JavaCode/JavaCode/src/test/java/leet/methods/binarySearch/easy/Leet278Test.java
@@ -0,0 +1,26 @@
+package leet.methods.binarySearch.easy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Leet278Test {
+    @ParameterizedTest
+    @MethodSource("provideTestCases")
+    void maximumTripletValue(int n,int expected) {
+        int res = new Leet278().firstBadVersion(n);
+        Assertions.assertEquals (expected, res);
+    }
+
+    private static Stream<Arguments> provideTestCases() {
+        return Stream.of(
+//                Arguments.of(new int[]{-1, 0, 3, 5, 9, 12, 3},9,4),
+                Arguments.of(3,1)
+        );
+    }
+}
