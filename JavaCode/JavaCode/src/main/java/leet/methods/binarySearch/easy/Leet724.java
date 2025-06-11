b90fa5e242f907d42c65bdf68d01cb07ca0e364d
RyseQ1u <460255780@qq.com>
2025-06-11 11:07:20 +0800

    Leet274  commit

---
+++ JavaCode/JavaCode/src/main/java/leet/methods/binarySearch/easy/Leet724.java
@@ -0,0 +1,44 @@
+package leet.methods.binarySearch.easy;
+
+import java.util.Arrays;
+
+public class Leet724 {
+
+    /**
+     * 遍历比较左右两边的数据大小
+     */
+    public int pivotIndex(int[] nums) {
+        //左右两边元素的和
+        int l=0,r= Arrays.stream(nums).sum();
+        for (int i = 0; i < nums.length; i++) {
+            //右边减去当前元素
+            r-=nums[i];
+            if(l==r){
+                return i;
+            }
+            //左边添加当前元素
+            l+=nums[i];
+        }
+        return -1;
+    }
+
+    /**
+     * 使用左边的和 * 2 等于总和
+     */
+    public int pivotIndex_V2(int[] nums) {
+        int s = 0;
+        for (int num : nums) {
+            s += num;
+        }
+        int leftS = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (leftS * 2 == s - nums[i]) {
+                return i;
+            }
+            leftS += nums[i];
+        }
+        return -1;
+
+    }
+
+}
