fda8b9167cf72a60b88b92eaf40d5924a3473a80
RyseQ1u <460255780@qq.com>
2025-06-16 12:18:14 +0800

    Leet236  commit

---
+++ JavaCode/JavaCode/src/main/java/leet/methods/tree/Leet236.java
@@ -0,0 +1,24 @@
+package leet.methods.tree;
+
+import leet._class.Tree.TreeNode;
+
+public class Leet236 {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        //判断是否为空
+        if(root==null) return root;
+        //判断是否找到了目标节点 向上返回
+        if(root == p || root == q) return root;
+        //递归左侧 若p,q在同侧则有返回值
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        //递归右侧
+        TreeNode rigth = lowestCommonAncestor(root.right,p,q);
+        //表示目标都不在左侧
+        if(left == null) return  rigth;
+        //表示目标都不在右侧
+        if(rigth == null) return  left;
+        //表示目标在根节点两侧
+        return root;
+    }
+
+}
