25b622c8521ec66827fb0cd1ef6ee4d4ef4678e6
RyseQ1u <460255780@qq.com>
2025-07-22 09:57:37 +0800

    动态规划 leet53 最大子数组和 commit

---
+++ JavaCode/JavaCode/src/main/java/leet/methods/_10_Dynamic/Leet53.java
@@ -0,0 +1,20 @@
+package leet.methods._10_Dynamic;
+
+/**
+ * 最大子数组和
+ */
+public class Leet53 {
+
+    /**
+     * 最大子数组和
+     * 维护前一位的最大和
+     */
+    public int maxSubArray(int[] nums) {
+        int max = nums[0],pre = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            pre = Math.max(nums[i],nums[i]+pre);
+            max = Math.max(max,pre);
+        }
+        return max;
+    }
+}
