4b7313bdef163bd5d471c31da6785b860828fcee
RyseQ1u <460255780@qq.com>
2025-07-29 10:05:10 +0800

    动态规划 leet300 最长递增子序列 commit

--- JavaCode/JavaCode/src/main/java/leet/methods/_10_Dynamic/Leet300.java
+++ JavaCode/JavaCode/src/main/java/leet/methods/_10_Dynamic/Leet300.java
@@ -10,7 +10,7 @@ public class Leet300 {
         if(nums.length==0) return 0;
         int[] dp = new int[nums.length];
         int res =0;
-        Arrays.fill(nums,1);
+        Arrays.fill(dp,1);
         for(int i = 0; i < nums.length; i++) {
             for(int j = 0; j < i; j++) {
                 if(nums[j] < nums[i]) dp[i] = Math.max(dp[i], dp[j] + 1);
