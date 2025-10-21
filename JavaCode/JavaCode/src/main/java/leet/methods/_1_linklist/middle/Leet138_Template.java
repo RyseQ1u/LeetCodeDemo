1a8a015074138624bb8e5b8ebffd53b46764546d
RyseQ1u <460255780@qq.com>
2025-10-21 14:25:11 +0800

    Leet 138 复习 commit

---
+++ JavaCode/JavaCode/src/main/java/leet/methods/_1_linklist/middle/Leet138_Template.java
@@ -0,0 +1,16 @@
+package leet.methods._1_linklist.middle;
+
+
+import leet._class.linklist.Node;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Leet138_Template {
+
+    /// 随机链表的复制
+    public Node copyRandomList(Node head) {
+        return null;
+    }
+
+}
---
+++ JavaCode/JavaCode/src/main/java/leet/methods/_1_linklist/middle/Leet138_r1.java
@@ -0,0 +1,28 @@
+package leet.methods._1_linklist.middle;
+
+
+import leet._class.linklist.Node;
+
+import java.util.HashMap;
+
+public class Leet138_r1 {
+
+    /// 随机链表的复制
+    public Node copyRandomList(Node head) {
+        if(head==null)return null;
+        Node cur = head;
+        HashMap<Node,Node> map = new HashMap<>();
+        while (cur!=null){
+            map.put(cur,new Node(cur.val));
+            cur= cur.next;
+        }
+        cur = head;
+        while (cur!=null){
+            map.get(cur).random =map.get(cur.random);
+            map.get(cur).next =map.get(cur.next);
+            cur=cur.next;
+        }
+        return map.get(head);
+    }
+
+}
