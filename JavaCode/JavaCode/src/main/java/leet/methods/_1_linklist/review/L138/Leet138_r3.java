8f50ea57cfd3ba61efe2595c74c81207f71b919d
RyseQ1u <460255780@qq.com>
2025-10-29 10:21:34 +0800

    Leet 138 复习 commit

--- JavaCode/JavaCode/src/main/java/leet/methods/_1_linklist/review/L138/Leet138_r3.java
+++ JavaCode/JavaCode/src/main/java/leet/methods/_1_linklist/review/L138/Leet138_r3.java
@@ -4,24 +4,28 @@ package leet.methods._1_linklist.review.L138;
 import leet._class.linklist.Node;
 
 import java.util.HashMap;
+import java.util.Map;
 
-public class Leet138_r2 {
+public class Leet138_r3 {
 
     /// 随机链表的复制
     public Node copyRandomList(Node head) {
-        ///记录新旧节点映射关系
+        // 记录旧节点到新节点的映射
         HashMap<Node, Node> map = new HashMap<>();
         Node cur = head;
-        ///复制链表
+        // 第一次遍历，复制节点并建立映射
         while (cur != null) {
             map.put(cur, new Node(cur.val));
             cur = cur.next;
         }
         cur = head;
-        /// 更新新节点的映射关系
+        // 第二次遍历，设置next和random指针
         while (cur != null) {
-            map.get(cur).next = map.get(cur.next);
-            map.get(cur).random = map.get(cur.random);
+            //新节点
+            Node newNode = map.get(cur);
+            // 设置next和random指针
+            newNode.next = map.get(cur.next);
+            newNode.random = map.get(cur.random);
             cur = cur.next;
         }
         return map.get(head);
