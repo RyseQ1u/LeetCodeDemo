fbe37e061eaa1175403a7a022085c5c1d3b9d361
RyseQ1u <460255780@qq.com>
2025-10-29 10:04:16 +0800

    Leet 266 复习 commit

---
+++ JavaCode/JavaCode/src/test/java/leet/methods/_3_hashTable/easy/review/L266/L266_r1Test.java
@@ -0,0 +1,30 @@
+package leet.methods._3_hashTable.easy.review.L266;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class L266_r1Test {
+
+    @ParameterizedTest
+    @MethodSource("provideTestCases")
+    void canPermutePalindrome(String s , boolean expected) {
+        boolean res = new L266_r1().canPermutePalindrome(s);
+        Assertions.assertEquals (expected, res);
+    }
+
+    private static Stream<Arguments> provideTestCases() {
+
+        return Stream.of(
+                Arguments.of("aab",true),
+                Arguments.of("carerac",true),
+                Arguments.of("code",false),
+                Arguments.of("a",true),
+                Arguments.of("ab",false)
+        );
+
+    }
+}
