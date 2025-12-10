0eea24963e3314e2f00910d025e58235a860c570
RyseQ1u <460255780@qq.com>
2025-12-10 15:27:49 +0800

    Leet 103 278 704 review commit

---
+++ JavaCode/JavaCode/src/main/java/leet/methods/_6_Search/review/L704/Leet704_r4.java
@@ -0,0 +1,19 @@
+package leet.methods._6_Search.review.L704;
+
+///  升序数组 二分查找
+public class Leet704_r4 {
+    public int search(int[] nums, int target) {
+        int l =0 ,r =nums.length-1;
+        while (l<=r){
+            int mid = l+(r-l)/2;
+            if(nums[mid]>target){
+                r=mid-1;
+            }else if(nums[mid]<target){
+                l=mid+1;
+            }else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
---
+++ JavaCode/JavaCode/src/main/java/leet/methods/_7_Tree/review/L103/Leet103_Template.java
@@ -0,0 +1,12 @@
+package leet.methods._7_Tree.review.L103;
+import leet._class.Tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/// 锯齿形 层序遍历
+public class Leet103_Template {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        return new ArrayList<>();
+    }
+}
