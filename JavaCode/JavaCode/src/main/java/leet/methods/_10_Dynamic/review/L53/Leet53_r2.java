558ca9051ab7cf6b975b0c1d8dceb435b7ff9725
RyseQ1u <460255780@qq.com>
2026-02-08 23:34:17 +0800

    Leet 53 64 review commit

---
+++ JavaCode/JavaCode/src/main/java/leet/methods/_10_Dynamic/review/L53/Leet53_r2.java
@@ -0,0 +1,13 @@
+package leet.methods._10_Dynamic.review.L53;
+
+public class Leet53_r2 {
+    public int maxSubArray(int[] nums) {
+        int res=nums[0],pre=nums[0];
+        for (int i = 1; i < nums.length; i++)
+        {
+            pre = Math.max(nums[i],pre+nums[i]);
+            res = Math.max(pre,res );
+        }
+        return res;
+    }
+}
