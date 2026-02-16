859868e1217cf061220d0b55f2afd83900e8eb9c
RyseQ1u <460255780@qq.com>
2026-02-16 23:42:15 +0800

    Leet 101 300 review commit

--- JavaCode/JavaCode/src/main/java/leet/methods/_10_Dynamic/review/L300/Leet300_r2.java
+++ JavaCode/JavaCode/src/main/java/leet/methods/_10_Dynamic/review/L300/Leet300_r2.java
@@ -4,17 +4,17 @@ import java.util.Arrays;
 
 public class Leet300_r2 {
     public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp,1);
-        int max =0;
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < i   ; j++) {
-                if(nums[i]>nums[j]){
-                    dp[i]=Math.max(dp[j]+1,dp[i]);
+            int[] dp = new int[nums.length];
+            Arrays.fill(dp, 1);
+            int max = 0;
+            for (int i = 0; i < nums.length; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (nums[i] > nums[j]) {
+                        dp[i] = Math.max(dp[j] + 1, dp[i]);
+                    }
                 }
+                max = Math.max(max, dp[i]);
             }
-            max=Math.max(max,dp[i]) ;
-        }
-        return max;
+            return max;
     }
 }
