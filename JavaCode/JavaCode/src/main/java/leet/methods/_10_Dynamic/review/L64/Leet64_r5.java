9d29c997ada63196d44903fa144ba7b6c845fd7b
RyseQ1u <460255780@qq.com>
2026-02-24 10:48:33 +0800

    Leet 64 264 review commit

---
+++ JavaCode/JavaCode/src/main/java/leet/methods/_10_Dynamic/review/L64/Leet64_r5.java
@@ -0,0 +1,16 @@
+package leet.methods._10_Dynamic.review.L64;
+
+public class Leet64_r5 {
+    public int minPathSum(int[][] grid) {
+        int n = grid.length, m = grid[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == 0 && j == 0) continue;
+                if (i == 0) grid[i][j] += grid[i][j - 1];
+                else if (j == 0) grid[i][j] += grid[i - 1][j];
+                else grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+            }
+        }
+        return grid[n - 1][m - 1];
+    }
+}
