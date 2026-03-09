214eb1f3167db2f944b2ac4f076a4c06eb384e86
RyseQ1u <460255780@qq.com>
2026-03-10 01:32:36 +0800

    Leet 1115 review commit

--- JavaCode/JavaCode/src/main/java/leet/methods/_14_Thread/L1115/LeetCode1115_Template.java
+++ JavaCode/JavaCode/src/main/java/leet/methods/_14_Thread/L1115/LeetCode1115_Template.java
@@ -27,4 +27,30 @@ public class LeetCode1115_Template {
             }
         }
     }
+
+    class FooBarV2 {
+        private final int n;
+
+        public FooBarV2(int n) {
+            this.n = n;
+        }
+
+        public void foo(Runnable printFoo) throws InterruptedException {
+
+            for (int i = 0; i < n; i++) {
+
+                // printFoo.run() outputs "foo". Do not change or remove this line.
+                printFoo.run();
+            }
+        }
+
+        public void bar(Runnable printBar) throws InterruptedException {
+
+            for (int i = 0; i < n; i++) {
+
+                // printBar.run() outputs "bar". Do not change or remove this line.
+                printBar.run();
+            }
+        }
+    }
 }
