ea1d03badeb3f428784099fef74c7a34024b7a5c
RyseQ1u <460255780@qq.com>
2026-03-24 22:32:01 +0800

    Leet 134 768 review commit

---
+++ JavaCode/JavaCode/src/main/java/leet/methods/_11_GreedyAlgorithm/L135/Leet134_r2.java
@@ -0,0 +1,23 @@
+package leet.methods._11_GreedyAlgorithm.L135;
+
+public class Leet134_r2 {
+    public int candy(int[] ratings) {
+        //双向贪心
+        int res=0,n=ratings.length;
+        int[] left = new int[n],right = new int[n];
+        for (int i = 0; i < n-1; i++) {
+            if(ratings[i+1]>ratings[i]){
+                left[i+1]=left[i]+1;
+            }
+        }
+        for (int i = n-1; i > 0; i--) {
+            if(ratings[i-1]>ratings[i]){
+                right[i-1]=right[i]+1;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            res+=Math.max(left[i],right[i]);
+        }
+        return res+n;
+    }
+}
---
+++ JavaCode/JavaCode/src/main/java/leet/methods/_11_GreedyAlgorithm/L768/Leet768_Template.java
@@ -0,0 +1,7 @@
+package leet.methods._11_GreedyAlgorithm.L768;
+
+public class Leet768_Template {
+    public int maxChunksToSorted(int[] arr) {
+        return 0;
+    }
+}
