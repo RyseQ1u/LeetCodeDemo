247cc9dfaeb9dd0016fc79cc07c63426baed7a66
RyseQ1u <460255780@qq.com>
2026-04-13 09:51:50 +0800

    Leet 11 231 review commit

--- JavaCode/JavaCode/src/main/java/leet/methods/_11_GreedyAlgorithm/L11/Leet11_r6.java
+++ JavaCode/JavaCode/src/main/java/leet/methods/_11_GreedyAlgorithm/L11/Leet11_r6.java
@@ -3,7 +3,16 @@ package leet.methods._11_GreedyAlgorithm.L11;
 public class Leet11_r6 {
     public int maxArea(int[] height) {
         //双指针
-
-        return 0;
+        int l =0,r = height.length-1,max=0;
+        while (l<r){
+            if(height[l]>height[r]){
+                max=Math.max(max,height[r]*(r-l));
+                r--;
+            }else {
+                max=Math.max(max,height[l]*(r-l));
+                l++;
+            }
+        }
+        return max;
     }
 }
