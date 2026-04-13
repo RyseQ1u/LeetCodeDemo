3972651231e114e2fcfe3e24eaf9a8dcdcd3794a
RyseQ1u <460255780@qq.com>
2026-04-13 09:36:40 +0800

    Leet 191 review commit

---
+++ JavaCode/JavaCode/src/main/java/leet/methods/_12_BitManipulation/L191/Leet191_r5.java
@@ -0,0 +1,12 @@
+package leet.methods._12_BitManipulation.L191;
+
+public class Leet191_r5 {
+    public int hammingWeight(int n) {
+        int res = 0;
+        while (n!=0){
+            if((n&1)==1)res++;
+            n>>=1;
+        }
+        return res;
+    }
+}
