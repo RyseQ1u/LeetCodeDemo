1fe3de56cf5d3942db540da723fd2e79ee7dac4e
RyseQ1u <460255780@qq.com>
2026-05-23 22:58:54 +0800

    Leet  232 242 295 394 review commit

---
+++ JavaCode/JavaCode/src/main/java/leet/methods/_3_hashTable/easy/review/L242/Leet242_r8.java
@@ -0,0 +1,17 @@
+package leet.methods._3_hashTable.easy.review.L242;
+
+/// 有效的字母异位词
+public class Leet242_r8 {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())return false;
+        int[] map = new int[127];
+        for (int i = 0; i < s.length(); i++) {
+            map[s.charAt(i)]++;
+            map[t.charAt(i)]--;
+        }
+        for (int i : map) {
+            if(i!=0)return false;
+        }
+        return true;
+    }
+}
