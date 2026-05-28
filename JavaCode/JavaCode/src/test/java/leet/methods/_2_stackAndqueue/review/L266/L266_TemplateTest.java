2a18c7101bb9d7c3dc14f06954a9e985de048e34
RyseQ1u <460255780@qq.com>
2026-05-28 23:01:08 +0800

    Leet 266 11 191 review commit

--- JavaCode/JavaCode/src/test/java/leet/methods/_2_stackAndqueue/review/L266/L266_TemplateTest.java
+++ JavaCode/JavaCode/src/test/java/leet/methods/_2_stackAndqueue/review/L266/L266_TemplateTest.java
@@ -11,7 +11,7 @@ class L266_Test {
     @ParameterizedTest
     @MethodSource("provideTestCases")
     void canPermutePalindrome(String s , boolean expected) {
-        boolean res = new L266_r5().canPermutePalindrome(s);
+        boolean res = new L266_r7().canPermutePalindrome(s);
         Assertions.assertEquals (expected, res);
     }
 
