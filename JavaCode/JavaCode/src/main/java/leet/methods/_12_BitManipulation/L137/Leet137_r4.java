e8cd6581890f171d54b2a3875d20446a90bb652a
RyseQ1u <460255780@qq.com>
2026-06-02 23:25:24 +0800

    Leet 137 169 review commit

---
+++ JavaCode/JavaCode/src/main/java/leet/methods/_12_BitManipulation/L137/Leet137_r4.java
@@ -0,0 +1,19 @@
+package leet.methods._12_BitManipulation.L137;
+
+public class Leet137_r4 {
+    public int singleNumber(int[] nums) {
+        int[] cnts = new int[32];
+        for (int num : nums) {
+            for (int i = 0; i < 32; i++) {
+                cnts[i]+=num&1;
+                num>>=1;
+            }
+        }
+        int res = 0,m=3;
+        for (int i = cnts.length - 1; i >= 0; i--) {
+            res<<=1;
+            res |= cnts[i]%m;
+        }
+        return res;
+    }
+}
