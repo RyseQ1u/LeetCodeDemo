01b656070c681ff8352b1c1e4925feaf58f22103
RyseQ1u <460255780@qq.com>
2026-06-24 23:41:46 +0800

    Leet 104 review commit

--- JavaCode/JavaCode/src/leet/methods/_8_Recall/review/L104/Leet104_Template.java
+++ JavaCode/JavaCode/src/leet/methods/_8_Recall/review/L104/Leet104_Template.java
@@ -2,14 +2,33 @@ package leet.methods._8_Recall.review.L104;
 
 
 import leet._class.Tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Leet104_Template {
     /// 递归实现
     public int maxDepth(TreeNode root) {
-        return 0;
+        if(root==null)return 0;
+        return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
     }
 
     /// 迭代实现
     public int maxDepth_bfs(TreeNode root) {
-        return 0;
+        if(root==null)return 0;
+        int res = 0;
+        LinkedList<TreeNode> nodes = new LinkedList<>();
+        nodes.add(root);
+        while (!nodes.isEmpty()){
+            LinkedList<TreeNode> curs = new LinkedList<>();
+            while (!nodes.isEmpty()){
+                TreeNode cur = nodes.pop();
+                if(cur.left!=null)curs.add(cur.left);
+                if(cur.right!=null)curs.add(cur.right);
+            }
+            nodes=curs;
+            res++;
+        }
+        return res;
     }
 }
